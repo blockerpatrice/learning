80daf1dca1c3f095fc7b9398f205521112b85662
Patrice Blocker <patricemblocker@gmail.com>
2020-07-16 14:47:14 -0600

    Custom hook not working properly from follow along

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,14 +1,22 @@
-import React, {useState} from 'react';
-import {ANIMALS} from '@frontendmasters/pet'
-
+import React, {useState, useEffect} from 'react'
+import pet, {ANIMALS} from '@frontendmasters/pet'
 
 const SearchParams = () => {
 
-    const [location,setLocation] = useState("Boise, ID") 
-    const [animal, setAnimal] = useState("dog")
+    const [location,setLocation] = useState("Seattle, WA") 
+    const [animal, Animal] = useState("dog")
     const [breed, setBreed] = useState("")
     const [breeds, setBreeds] = useState([])
 
+    useEffect(() =>{
+        setBreeds([])
+        setBreed("")
+
+        pet.breeds(animal).then(({breeds}) => {
+            const breedStrings = breeds.map(({name}) => name)
+            setBreeds(breedStrings)
+        }, console.error)
+    })
     return (
         <div className="search-params">
             <form>
@@ -18,14 +26,16 @@ const SearchParams = () => {
                     id="location" 
                     value={location} 
                     placeholder="location" 
+                    //Whenever ther is an event, setLocation to e.target.value
                     onChange={e =>setLocation(e.target.value)}/>
                 </label>
                 <label htmlFor="animal">
-                    animal
+                    Animal
                     <select
                         id="animal"
                         value={animal}
                         onChange={e => setAnimal(e.target.value)}
+                        //some screen readers do not register onChange, so we will also use inBlur
                         onBlur={e => setAnimal(e.target.value)}>
                         <option>All</option>
                         {ANIMALS.map(animal => {
